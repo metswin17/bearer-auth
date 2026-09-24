e122a4256a224b2d0e9d07ec9f0cf3eb69ef5e1f
Luis Lopez <metfanforever@gmail.com>
2026-09-24 00:41:03 -0700

    fix authentication starter code

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -1,17 +1,20 @@
 'use strict';
 
 const base64 = require('base-64');
-const { user } = require('../models/index.js');
+const { users } = require('../models/index.js');
 
 module.exports = async (req, res, next) => {
 
-  if (!req.headers.authorization) { return _authError(); }
+  if (!req.headers.authorization) {
+    return res.status(403).send('Invalid Login');
+  }
 
-  let basic = req.headers.authorization;
-  let [username, pass] = base64.decode(basic).split(':');
+  let basic = req.headers.authorization.split(' ').pop();
+
+let [username, pass] = base64.decode(basic).split(':');
 
   try {
-    req.user = await user.authenticateBasic(username, pass)
+    req.user = await users.authenticateBasic(username, pass);
     next();
   } catch (e) {
     console.error(e);
